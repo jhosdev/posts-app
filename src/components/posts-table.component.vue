28e1e23d74bba518d4a7b5a317839a4b8fa90514
Jhosaim <86936147+jhosR@users.noreply.github.com>
2023-04-27 01:13:12 -0500

    delete functionality

--- src/components/posts-table.component.vue
+++ src/components/posts-table.component.vue
@@ -4,8 +4,8 @@
             <pv-column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></pv-column>
             <pv-column style="width: 5rem">
                 <template #body="slotProps">
-                    <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editProduct(slotProps.data)" />
-                    <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(slotProps.data)" />
+                    <pv-button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editPost(slotProps.data)" />
+                    <pv-button icon="pi pi-trash" outlined rounded severity="danger" @click="deletePost(slotProps.data.id)" />
                 </template>
             </pv-column>
         </DataTable>
@@ -16,14 +16,32 @@
 import {ref, onMounted, onBeforeMount} from 'vue';
 import {PostsApiService} from "@/services/posts-api-services";
 
+let postsService = new PostsApiService();
+
 onBeforeMount(() => {
-    let postsService = new PostsApiService();
+    getPosts();
+});
+
+const getPosts = () => {
     postsService.getAll().then((response) => {
         console.log(response)
         console.log(response.data)
         posts.value = response.data
-});
-});
+    });
+};
+const deletePost = (id) => {
+    console.log(id)
+    postsService.delete(id).then((response) => {
+        console.log(response);
+        if(response.status === 200){
+            console.log("post deleted")
+            this.getPosts();
+        }
+        else(
+            console.log("error deleting post")
+        )
+    });
+};
 
 const posts = ref();
 const columns = [
